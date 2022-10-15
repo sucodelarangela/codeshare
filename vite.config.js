ca8af86ae04cff9b0590c145e86b86e8bf9c2194
Angela Caldas <aes.caldas@gmail.com>
2022-10-15 20:41:46 -0300

    feat: vite app init

---
+++ vite.config.js
@@ -0,0 +1,10 @@
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import jsconfigPaths from 'vite-jsconfig-paths';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    jsconfigPaths()
+  ],
+});
