1914b325993cb8777275d883811b4663d37196b8
Angela Caldas <aes.caldas@gmail.com>
2022-10-30 15:58:40 -0300

    chore: testing routes with standard node methods

--- server/server.js
+++ server/server.js
@@ -1,11 +1,18 @@
 const http = require('http');
 const port = 8000;
 
+const routes = {
+  '/': 'Codeshare server',
+  '/codes': 'Codes saved',
+  '/users': 'Users'
+};
+
 // Criando o servidor e definindo o que ele vai escutar
 const server = http.createServer((req, res) => {
   // status, {tipo de conteúdo}
-  res.writeHead(200, { 'Content-Type': 'text/plain' });
-  res.end('Curso de Node'); // conteúdo renderizado
+  res.writeHead(200, { 'Content-Type': 'text/plain' }); // configurando cabeçalho
+  //res.end('Curso de Node'); // conteúdo renderizado estático em tela ↓
+  res.end(routes[req.url]); // mostra conteúdo de acordo com a url
 });
 
 server.listen(port, () => {
