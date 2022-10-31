3a04dff3d30057542f3f1317f4c730e1aa2c44a2
Angela Caldas <aes.caldas@gmail.com>
2022-10-31 18:18:06 -0300

    feat: add new basic routes to app.js

--- server/src/app.js
+++ server/src/app.js
@@ -2,6 +2,8 @@
 import express from 'express';
 const app = express();
 
+app.use(express.json()); // para conseguir interpretar o que vem via POST ou PUT como json
+
 const codes = [
   { id: 1, title: '<p>Hello World</p>' },
   { id: 2, title: '<h1>Title</h1>' }
@@ -15,5 +17,33 @@ app.get('/', (req, res) => {
 app.get('/codes', (req, res) => {
   res.status(200).json(codes);
 });
+
+app.get('/codes/:id', (req, res) => {
+  let index = findCode(req.params.id);
+  res.json(codes[index]);
+});
+
+app.post('/codes', (req, res) => {
+  codes.push(req.body); // acrescenta no array o que vier no corpo da requisição
+  res.status(201).send('O código foi cadastrado com sucesso.');
+});
+
+app.put('/codes/:id', (req, res) => {
+  let index = findCode(req.params.id);
+  codes[index].title = req.body.title;
+  res.json(codes);
+});
+
+app.delete('/codes/:id', (req, res) => {
+  let { id } = req.params; // desestruturação
+  let index = findCode(id);
+  codes.splice(index, 1);
+  res.send(`Código ${id} removido com sucesso!`);
+});
+
+// função para o put: temporária pois ainda não estamos com o banco de dados implementado
+function findCode(id) {
+  return codes.findIndex(code => code.id == id);
+}
 
 export default app;
