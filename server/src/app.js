7abef50d08dc30462137b80013560d962f323847
Angela Caldas <aes.caldas@gmail.com>
2022-11-05 20:19:09 -0300

    refactor: remove unnecessary or duplicate codes

--- server/src/app.js
+++ server/src/app.js
@@ -1,7 +1,6 @@
 // configurando o uso do express
 import express from 'express';
 import db from './config/dbConnect.js'; // importando o db
-import codes from './models/Code.js'; // importando o schema
 import routes from './routes/index.js';
 
 // se ocorrer algum erro na conexão, fazemos um link com o nosso terminal para mostrar esse erro
@@ -17,51 +16,5 @@ const app = express();
 app.use(express.json()); // para conseguir interpretar o que vem via POST ou PUT como json
 
 routes(app);
-
-// const codes = [
-//   { id: 1, title: '<p>Hello World</p>' },
-//   { id: 2, title: '<h1>Title</h1>' }
-// ];
-
-// requisições:
-// app.get('/', (req, res) => {
-//   res.status(200).send('Codeshare');
-// });
-
-// VAI PARA O CODESCONTROLLER.JS
-// app.get('/codes', (req, res) => {
-//   // find -> traz todos os elementos
-//   codes.find((err, codes) => {
-//     res.status(200).json(codes);
-//   });
-// });
-
-app.get('/codes/:id', (req, res) => {
-  let index = findCode(req.params.id);
-  res.json(codes[index]);
-});
-
-// app.post('/codes', (req, res) => {
-//   codes.push(req.body); // acrescenta no array o que vier no corpo da requisição
-//   res.status(201).send('O código foi cadastrado com sucesso.');
-// });
-
-app.put('/codes/:id', (req, res) => {
-  let index = findCode(req.params.id);
-  codes[index].title = req.body.title;
-  res.json(codes);
-});
-
-app.delete('/codes/:id', (req, res) => {
-  let { id } = req.params; // desestruturação
-  let index = findCode(id);
-  codes.splice(index, 1);
-  res.send(`Código ${id} removido com sucesso!`);
-});
-
-// função para o put: temporária pois ainda não estamos com o banco de dados implementado
-function findCode(id) {
-  return codes.findIndex(code => code.id == id);
-}
 
 export default app;
