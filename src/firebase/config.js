76ee50ba507a2bf1b85efef11a04913797fceb54
Angela Caldas <aes.caldas@gmail.com>
2022-11-17 11:41:20 -0300

    build: add firebase dependency and set firebase config.js

---
+++ src/firebase/config.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from 'firebase/app';
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: 'AIzaSyDzOTGHJS0vCKoya6ESqtStLUTB4xSTK-Q',
+  authDomain: 'codeshare-af721.firebaseapp.com',
+  projectId: 'codeshare-af721',
+  storageBucket: 'codeshare-af721.appspot.com',
+  messagingSenderId: '897869565755',
+  appId: '1:897869565755:web:704a92a300c65195d5311e'
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
