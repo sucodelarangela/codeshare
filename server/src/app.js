34a7c78b6b7c5032dfd526a9d99fb698dd177cda
Angela Caldas <aes.caldas@gmail.com>
2022-12-02 11:14:56 -0300

    chore: config headers to allow crossorigin

--- server/src/app.js
+++ server/src/app.js
@@ -15,6 +15,12 @@ const app = express();
 
 app.use(express.json()); // para conseguir interpretar o que vem via POST ou PUT como json
 
+app.use(function (req, res, next) {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
+
 routes(app);
 
 export default app;
