7010d55ba9d0c25cce8895749ee439f6a8714354
Angela Caldas <aes.caldas@gmail.com>
2022-12-20 14:12:23 -0300

    refactor: show highlight themes on editor form

--- src/pages/Home/CustomizationForm.jsx
+++ src/pages/Home/CustomizationForm.jsx
@@ -1,6 +1,8 @@
 import * as Styled from './styles';
+import * as hljs from 'react-syntax-highlighter/dist/esm/styles/hljs';
 
 export const CustomizationForm = ({ color, setColor }) => {
+  const hljsKeys = Object.keys(hljs);
   return (
     <Styled.Form>
       <Styled.Fieldset>
@@ -13,14 +15,19 @@ export const CustomizationForm = ({ color, setColor }) => {
       <Styled.Fieldset>
         <h3>Personalização</h3>
         <div>
-          <label htmlFor='project__language' className='sr-only'>Escolha a linguagem do seu código</label>
-          <select name="languages" id="project__language" defaultValue='Selecione uma linguagem'>
-            <option value="Selecione uma linguagem" disabled>Selecione uma linguagem</option>
-            <option value="javascript">JavaScript</option>
+          <label htmlFor='project__language' className='sr-only'>Digite a linguagem do seu projeto</label>
+          <input id='project__language' type='text' placeholder='Linguagem do seu projeto' />
+          <label htmlFor='project__highlight' className='sr-only'>Escolha a linguagem do seu código</label>
+          <select name="highlight" id="project__highlight" defaultValue='Selecione um highlight'>
+            <option value="Selecione um tema de highlight" disabled>Selecione um tema de highlight</option>
+            {hljsKeys.map((key) => (
+              <option key={key} value={key}>{key}</option>
+            ))}
+            {/* <option value="javascript">JavaScript</option>
             <option value="java">Java</option>
             <option value="react">React</option>
             <option value="json">JSON</option>
-            <option value="typescript">TypeScript</option>
+            <option value="typescript">TypeScript</option> */}
           </select>
           <label htmlFor='project__color' className='sr-only'>Selecione uma cor para o seu card</label>
           <input id='project__color' type='color' value={color} onChange={(e) => setColor(e.target.value)} />
