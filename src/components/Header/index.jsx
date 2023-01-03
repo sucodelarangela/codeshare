f472de578bac6c2d2066f437fa2df844522b156e
Angela Caldas <aes.caldas@gmail.com>
2023-01-03 17:33:06 -0300

    refactor: show search input only on Community page

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -10,12 +10,14 @@ import { useState } from 'react';
 import { useAuthValue } from 'context/AuthContext.jsx';
 import Menu from './Menu.jsx';
 import { useSearchValue } from 'context/SearchContext.jsx';
+import { useLocation } from 'react-router-dom';
 
 export const Header = () => {
   const [showDialog, setShowDialog] = useState(false);
   const [showMenu, setShowMenu] = useState(false);
   const { user } = useAuthValue();
   const { query, setQuery } = useSearchValue();
+  const { pathname } = useLocation();
 
   function toggleDialog() {
     setShowDialog((showDialog) => !showDialog);
@@ -27,16 +29,19 @@ export const Header = () => {
 
   return (
     <Styled.Header>
-      {/* <img src={logo} alt="Codeshare home" /> */}
       <h1 className='home__title'>codeshare</h1>
-      <label htmlFor="home__search" className='sr-only'>Busque por algo</label>
-      <Styled.Search
-        id='home__search'
-        type='text'
-        placeholder='Busque por algo...'
-        value={query}
-        onInput={(e) => setQuery(e.target.value)}
-      />
+      {pathname === '/' ? (
+        <>
+          <label htmlFor="home__search" className='sr-only'>Busque por algo</label>
+          <Styled.Search
+            id='home__search'
+            type='text'
+            placeholder='Busque por algo...'
+            value={query}
+            onInput={(e) => setQuery(e.target.value)}
+          />
+        </>
+      ) : ''}
       <Styled.MenuWrapper>
         <button><IoSearch size={32} /></button>
         <Hamburger size={32} label='Abrir menu' toggled={showMenu} toggle={toggleMenu} />
